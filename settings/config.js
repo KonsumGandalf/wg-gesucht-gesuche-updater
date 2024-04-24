d15570140bb88ba679fab846bd0ae7de0b6e1e5a
KonsumGandalf <david_schmidt_privat@outlook.com>
2024-04-24 16:54:21 +0200

    feat: update files

---
+++ settings/config.js
@@ -0,0 +1,12 @@
+config = {
+  login: {
+    username: "mail", // Your email address
+    password: "password", // Your password
+  },
+  offer: {
+    id: "id", // Your offer ID
+    interval: 20, // Update interval in minutes
+  },
+};
+
+module.exports = config;
